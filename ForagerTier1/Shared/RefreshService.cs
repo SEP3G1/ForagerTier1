15104eb8f228775f849fd607a60298bab841a974
Patrick <50921587+patrickdurke@users.noreply.github.com>
2020-11-27 13:49:15 +0100

    Closer

---
+++ ForagerTier1/Shared/RefreshService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ForagerTier1.Shared
+{
+    public class RefreshService : IRefreshService
+    {
+        public event Action RefreshRequested;
+        public void CallRequestRefresh()
+        {
+            Console.WriteLine("CallRequestRefresh()");
+            RefreshRequested?.Invoke();
+        }
+    }
+}
